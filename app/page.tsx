4b152727779e1890e283654ea91ace6ee234b3dd
Matheus Luís <matheus.bernardi@lnls.br>
2023-08-24 15:18:52 -0300

    removing ssr on components using window

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,22 @@
-import { Canvas } from "@/components/canvas";
-import { Toolbar } from "@/components/toolbar";
+"use client";
+// import { Canvas } from "@/components/canvas";
+// import { Toolbar } from "@/components/toolbar";
+import dynamic from "next/dynamic";
+
+const Canvas = dynamic(async () => {
+  const { Canvas: Component } = await import("@/components/canvas");
+  return { default: Component };
+}, {
+  ssr: false,
+});
+
+const Toolbar = dynamic(async () => {
+  const { Toolbar: Component } = await import("@/components/toolbar");
+  return { default: Component };
+}, {
+  ssr: false,
+  loading: () => <div>Loading...</div>,
+});
 
 export default function Home() {
   return (

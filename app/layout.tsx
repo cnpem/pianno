1c0195cbdf231fb79ec9a9cccbb32efc4388546a
Matheus Luís <matheus.bernardi@lnls.br>
2023-11-01 11:56:31 -0300

    tryin geist font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,12 @@
-import type { Metadata } from "next";
+import type { Metadata } from 'next';
 
-import { Inter } from "next/font/google";
+import { GeistSans } from 'geist/font';
 
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import './globals.css';
 
 export const metadata: Metadata = {
-  description: "Pimega Annotator",
-  title: "Pianno",
+  description: 'Pimega Annotator',
+  title: 'Pianno',
 };
 
 export default function RootLayout({
@@ -18,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={GeistSans.className}>{children}</body>
     </html>
   );
 }

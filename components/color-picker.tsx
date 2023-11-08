414fb3e8fed34c641cd6f3c5abaa64f527c1b4a2
Matheus Luís <matheus.bernardi@lnls.br>
2023-11-08 11:36:55 -0300

    remove unused imports and comments

--- components/color-picker.tsx
+++ components/color-picker.tsx
@@ -5,7 +5,7 @@ import {
   useStoreColors,
   useStoreCurrentColor,
 } from '@/hooks/use-store';
-import { cn, getAnnotationTypeFromColor } from '@/lib/utils';
+import { cn } from '@/lib/utils';
 import { FC } from 'react';
 
 import { Input } from './ui/input';
@@ -19,8 +19,6 @@ const ColorPicker: FC<ColorPickerProps> = ({}) => {
 
   return (
     <div className="grid grid-cols-4 gap-2 px-2">
-      {// color picker input for custom color
-      }
       <Input
         className="col-span-4"
         onChange={(e) => setColor(e.target.value)}
@@ -28,21 +26,19 @@ const ColorPicker: FC<ColorPickerProps> = ({}) => {
         value={currentColor}
       />
       {colors.map((color) => (
- 
-          <div
-            className={cn(
-              color === currentColor && 'border-2',
-              'h-4 w-4 cursor-pointer rounded-full transition-all hover:scale-125',
-            )}
-            key={color}
-            onClick={() => setColor(color)}
-            style={{
-              backgroundColor: color === currentColor ? 'transparent' : color,
-              borderColor: color,
-            }}
-            title={getAnnotationTypeFromColor(color)}
-          ></div>
-
+        <div
+          className={cn(
+            color === currentColor && 'border-2',
+            'h-4 w-4 cursor-pointer rounded-full transition-all hover:scale-125',
+          )}
+          key={color}
+          onClick={() => setColor(color)}
+          style={{
+            backgroundColor: color === currentColor ? 'transparent' : color,
+            borderColor: color,
+          }}
+          title={color}
+        ></div>
       ))}
     </div>
   );

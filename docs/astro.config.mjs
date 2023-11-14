3e8cc945298d5ede2b2aa58e6cd011999d1deacf
Matheus Luís <matheus.bernardi@lnls.br>
2023-11-14 09:49:13 -0300

    broken links in docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -17,7 +17,7 @@ export default defineConfig({
           label: 'Guides',
           items: [
             // Each item here is one entry in the navigation menu.
-            { label: 'Example Guide', link: 'pianno/guides/example/' },
+            { label: 'Example Guide', link: '/guides/example/' },
           ],
         },
         {

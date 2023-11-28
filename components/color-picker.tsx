b35bf3a6ca22283fe18d444c4555eaa19e384731
Matheus Luís <matheus.bernardi@lnls.br>
2023-11-28 14:10:06 -0300

    feat: new colorpicker

--- components/color-picker.tsx
+++ components/color-picker.tsx
@@ -6,13 +6,14 @@ import {
   useStoreCurrentColor,
 } from '@/hooks/use-store';
 import { cn } from '@/lib/utils';
+import { PopoverContent, PopoverTrigger } from '@radix-ui/react-popover';
 import { MoveDiagonal, MoveHorizontal, MoveVertical } from 'lucide-react';
 import { FC } from 'react';
+import { HexColorPicker } from 'react-colorful';
 import { useDebouncedCallback } from 'use-debounce';
 
-import { buttonVariants } from './ui/button';
-import { Input } from './ui/input';
-import { Label } from './ui/label';
+import { Button } from './ui/button';
+import { Popover } from './ui/popover';
 
 interface ColorPickerProps {}
 
@@ -27,48 +28,9 @@ const ColorPicker: FC<ColorPickerProps> = ({}) => {
 
   return (
     <div className="grid grid-cols-3 gap-1 p-2">
-      <Label
-        className={cn(
-          buttonVariants({ size: 'icon', variant: 'outline' }),
-          'h-6 w-6 cursor-pointer',
-        )}
-      >
-        <MoveVertical className="h-4 w-4" />
-        <Input
-          className="sr-only"
-          defaultValue={verticalColors.slice(-1)}
-          onChange={(e) => debounced(e.target.value, 'verticalColors')}
-          type="color"
-        />
-      </Label>
-      <Label
-        className={cn(
-          buttonVariants({ size: 'icon', variant: 'outline' }),
-          'h-6 w-6 cursor-pointer',
-        )}
-      >
-        <MoveHorizontal size={16} />
-        <Input
-          className="sr-only"
-          defaultValue={horizontalColors.slice(-1)}
-          onChange={(e) => debounced(e.target.value, 'horizontalColors')}
-          type="color"
-        />
-      </Label>
-      <Label
-        className={cn(
-          buttonVariants({ size: 'icon', variant: 'outline' }),
-          'h-6 w-6 cursor-pointer',
-        )}
-      >
-        <MoveDiagonal size={16} />
-        <Input
-          className="sr-only"
-          defaultValue={euclideanColors.slice(-1)}
-          onChange={(e) => debounced(e.target.value, 'euclideanColors')}
-          type="color"
-        />
-      </Label>
+      <PopoverPicker colorLabel="verticalColors" />
+      <PopoverPicker colorLabel="horizontalColors" />
+      <PopoverPicker colorLabel="euclideanColors" />
       <div className="col-span-1 ml-1" id="vertical-pallete">
         <div className="space-y-1">
           {verticalColors.map((color) => (
@@ -127,6 +89,63 @@ const ColorPicker: FC<ColorPickerProps> = ({}) => {
         </div>
       </div>
     </div>
+  );
+};
+
+const PopoverPicker = ({
+  colorLabel,
+}: {
+  colorLabel: 'euclideanColors' | 'horizontalColors' | 'verticalColors';
+}) => {
+  const { euclideanColors, horizontalColors, verticalColors } =
+    useStoreColors();
+  const { setNewColor } = useStoreActions();
+  const debounced = useDebouncedCallback((value) => {
+    setNewColor(value, colorLabel);
+  }, 300);
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button className="h-6 w-6" size={'icon'} variant="outline">
+          {colorLabel === 'verticalColors' && (
+            <MoveVertical className="h-4 w-4" />
+          )}
+          {colorLabel === 'horizontalColors' && (
+            <MoveHorizontal className="h-4 w-4" />
+          )}
+          {colorLabel === 'euclideanColors' && (
+            <MoveDiagonal className="h-4 w-4" />
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent
+        align="start"
+        className="h-32 w-32 rounded-lg border bg-background p-1 shadow-lg"
+        side="top"
+      >
+        {colorLabel === 'euclideanColors' && (
+          <HexColorPicker
+            color={euclideanColors.slice(-1)[0]}
+            onChange={debounced}
+            style={{ height: '100%', width: '100%' }}
+          />
+        )}
+        {colorLabel === 'horizontalColors' && (
+          <HexColorPicker
+            color={horizontalColors.slice(-1)[0]}
+            onChange={debounced}
+            style={{ height: '100%', width: '100%' }}
+          />
+        )}
+        {colorLabel === 'verticalColors' && (
+          <HexColorPicker
+            color={verticalColors.slice(-1)[0]}
+            onChange={debounced}
+            style={{ height: '100%', width: '100%' }}
+          />
+        )}
+      </PopoverContent>
+    </Popover>
   );
 };
 

e08d6693377b5278bdb6fab865b70fada10cd5d0
Matheus Luís <matheus.bernardi@lnls.br>
2023-12-19 12:05:21 -0300

    turn help into a dialog

--- components/help.tsx
+++ components/help.tsx
@@ -1,25 +1,200 @@
 import { cn } from '@/lib/utils';
+import { ExternalLink } from 'lucide-react';
 import Link from 'next/link';
 import { FC } from 'react';
+import { useReducer } from 'react';
+import { useHotkeys } from 'react-hotkeys-hook';
 
-import { buttonVariants } from './ui/button';
+import { Button, buttonVariants } from './ui/button';
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from './ui/dialog';
 
 interface HelpProps {}
 
 const Help: FC<HelpProps> = ({}) => {
+  const [open, toggleOpen] = useReducer((s) => !s, false);
+  useHotkeys('h', () => toggleOpen());
   return (
-    <Link
-      className={cn(
-        buttonVariants({ size: 'icon', variant: 'outline' }),
-        'fixed bottom-1 right-1 z-10 opacity-90 shadow-lg',
-      )}
-      href="https://cnpem.github.io/pianno/"
-      rel="noopener noreferrer"
-      target="_blank"
-      title="help"
-    >
-      ?
-    </Link>
+    <Dialog onOpenChange={toggleOpen} open={open}>
+      <DialogTrigger asChild>
+        <Button
+          className="fixed bottom-1 right-1 z-10 opacity-90 shadow-lg"
+          size="icon"
+          title="help"
+          variant="outline"
+        >
+          ?
+        </Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle>Help</DialogTitle>
+        </DialogHeader>
+        <div className="flex flex-col gap-4">
+          <div className="flex flex-row items-start justify-between gap-1">
+            <Link
+              className={cn(buttonVariants({ variant: 'outline' }), '')}
+              href="https://cnpem.github.io/pianno/"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="help"
+            >
+              Documentation
+              <ExternalLink className="ml-1" size={16} />
+            </Link>
+            <p className="mt-1 text-xl text-input"> | </p>
+            <Link
+              className={cn(buttonVariants({ variant: 'outline' }), '')}
+              href="https://github.com/cnpem/pianno/issues"
+              rel="noopener noreferrer"
+              target="_blank"
+              title="help"
+            >
+              Report an issue
+              <ExternalLink className="ml-1" size={16} />
+            </Link>
+          </div>
+          <p className="text-sm font-semibold">Keyboard shortcuts</p>
+          <div className="flex flex-col gap-2 rounded-md border border-input p-2">
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Drag</p>
+              <div className="flex flex-row items-center gap-1">
+                <p className="text-xs">hold</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  Middle
+                </kbd>
+                <p className="text-xs">or</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  Right
+                </kbd>
+                <p className="text-xs">mouse button</p>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Zoom</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  Scroll
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Pen</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  1
+                </kbd>
+                <p className="text-xs">or</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  p
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Eraser</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  2
+                </kbd>
+                <p className="text-xs">or</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  e
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Load Image</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  3
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Save Annotations</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  4
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Toggle pairs view</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  5
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Undo</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  6
+                </kbd>
+                <p className="text-xs">or</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  Ctrl
+                </kbd>
+                <p className="text-xs">+</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  z
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Redo</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  7
+                </kbd>
+                <p className="text-xs">or</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  Ctrl
+                </kbd>
+                <p className="text-xs">+</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  y
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Fit view</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  8
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Eraser size</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  up
+                </kbd>
+                <p className="text-xs">/</p>
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  down
+                </kbd>
+              </div>
+            </div>
+            <div className="flex flex-row items-center justify-between gap-1">
+              <p className="text-sm">Help</p>
+              <div className="flex flex-row items-center gap-1">
+                <kbd className="rounded-sm bg-violet-200 px-2 py-1 text-xs">
+                  H
+                </kbd>
+              </div>
+            </div>
+          </div>
+        </div>
+      </DialogContent>
+    </Dialog>
   );
 };
 
